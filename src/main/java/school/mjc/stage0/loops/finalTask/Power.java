9dbd991a59f6c18d5cdd3a7129c9f05d029c6ba9
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-10-15 16:37:17 +0300

    S

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int multiply = 1;
+        for(int i = 0; i < power; i++){
+            multiply *= numberToPrint;
+        }
+        System.out.println(multiply);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,30 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        /*for(int i = 1; i <= cathetusLength; i++){
+            for (int  j = 1; j <= cathetusLength * 2 - 1; j++){
+                if(j <= cathetusLength - i || j >= cathetusLength + i)
+                    System.out.print(" ");
+                else
+                    System.out.print(i);
+            }
+            System.out.println();
+        }*/
+        for (int i = 1; i <= cathetusLength; i++) {
 
+            for (int j = 1; j <= cathetusLength - i; j++)
+                System.out.print(" ");
+
+            for (int k = i; k >= 1; k--)
+                System.out.print(k);
+
+            for (int k = 2; k <=i; k++)
+                System.out.print(k);
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(9);
     }
 }
